839d83ba6548aea5e746062b5cc2f192fed8de05
hafsamulla38 <164649901+hafsamulla38@users.noreply.github.com>
2024-03-27 20:01:21 +0530

    Add files via upload

---
+++ pro 4.c
@@ -0,0 +1,14 @@
+//multipliction
+#include<stdio.h>
+#include<conio.h>
+ main()
+{
+	int a,b,c;
+	printf("enter the number a");
+	scanf("%d",&a);
+	printf("enter the number b");
+	scanf("%d",&b);
+	c=a*b;
+	printf("the mul=%d",c);
+	getch();
+}

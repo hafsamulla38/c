997ec28b902a1fdb1a802d52f70bcf4ad5219667
hafsamulla38 <164649901+hafsamulla38@users.noreply.github.com>
2024-03-29 20:38:08 +0530

    Add files via upload

---
+++ pro 5.c
@@ -0,0 +1,14 @@
+//dividection
+#include<stdio.h>
+#include<conio.h>
+ main()
+{
+	int a,b,c;
+	printf("enter the number a");
+	scanf("%d",&a);
+	printf("enter the number b");
+	scanf("%d",&b);
+	c=a/b;
+	printf("the div=%d",c);
+	getch();
+}

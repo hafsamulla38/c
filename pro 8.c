bfbe8b314880800fa3e057f5180017656c248ae6
hafsamulla38 <164649901+hafsamulla38@users.noreply.github.com>
2024-04-01 21:34:03 +0530

    Add files via upload

---
+++ pro 8.c
@@ -0,0 +1,13 @@
+//area of circle
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int radius;
+	float pie=10,b;
+	printf("enter the radius");
+	scanf("%d",&radius);
+	b=pie*radius*radius;
+	printf("area of circle=%f",b);
+	getch();
+}

e2b58cb7f40e928ea2adc6c805e15de5887c70fb
Hafsa Bashir Mulla <164649901+hafsamulla38@users.noreply.github.com>
2024-04-05 21:45:04 +0530

    Add files via upload

---
+++ if statment.c
@@ -0,0 +1,16 @@
+//if statment
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int number;
+	printf("enter the number");
+	scanf("%d",&number);
+	//true if number is less than 0
+	if(number<0);
+	{
+	printf("you entered %d",number);
+}
+	printf("the if statment is easy");
+    return 0;
+}

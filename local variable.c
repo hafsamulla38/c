298fe4ebe860654ae223b99576327f804b3aca4e
Hafsa Bashir Mulla <164649901+hafsamulla38@users.noreply.github.com>
2024-05-02 21:14:51 +0530

    Add files via upload

---
+++ local variable.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main (void)
+{
+//	int i;
+//	for(int i=0;i<5;++i)
+	{
+		printf("c programming");
+	}
+	//error is not declared at pointer
+	//printf("%d",i);
+	return 0;
+}
